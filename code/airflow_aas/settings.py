7f121dad9030b4105a97d5a7145128b2ab2ed075
Richard Hayes <richard.hayes@airfinity.com>
2018-04-07 23:03:11 +0100

    updated settings

--- code/airflow_aas/settings.py
+++ code/airflow_aas/settings.py
@@ -138,7 +138,7 @@ STATICFILES_DIRS = (
 WEBPACK_LOADER = {
     'DEFAULT': {
         'BUNDLE_DIR_NAME': 'bundles/',
-        'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json'),
+        'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.dev.json'),
     }
 }
 
