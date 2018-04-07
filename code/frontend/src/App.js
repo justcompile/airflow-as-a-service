79e0ac6f3b2a32c7d1097db19543571155c8bfea
Richard Hayes <richard.hayes@airfinity.com>
2018-04-07 23:41:21 +0100

    playing with react

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Route, Switch, BrowserRouter} from 'react-router-dom';
+
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+import ponyApp from "./reducers";
+
+import PonyNote from "./components/PonyNote";
+import NotFound from "./components/NotFound";
+
+let store = createStore(ponyApp, applyMiddleware(thunk));
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Provider store={store}>
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact path="/" component={PonyNote} />
+                        <Route component={NotFound} />
+                    </Switch>
+                </BrowserRouter>
+            </Provider>
+        );
+    }
 }
 
 export default App;
