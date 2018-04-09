19b490f20558a08d9150dea6238ae9346ecae7aa
Richard Hayes <richard.hayes@airfinity.com>
2018-04-09 22:32:57 +0100

    styling joy

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -5,6 +5,11 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import baseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 import airflowAsAServiceApp from "./reducers";
 
 import RepoList from "./components/RepoList";
@@ -13,18 +18,23 @@ import NotFound from "./components/NotFound";
 
 let store = createStore(airflowAsAServiceApp, applyMiddleware(thunk));
 
+const muiTheme = getMuiTheme(baseTheme);
+  
+
 class App extends Component {
     render() {
         return (
-            <Provider store={store}>
-                <BrowserRouter>
-                    <Switch>
-                        <Route exact path="/" component={Clusters} />
-                        <Route exact path="/repos" component={RepoList} />
-                        <Route component={NotFound} />
-                    </Switch>
-                </BrowserRouter>
-            </Provider>
+            <MuiThemeProvider muiTheme={muiTheme}>
+                <Provider store={store}>
+                    <BrowserRouter>
+                        <Switch>
+                            <Route exact path="/" component={Clusters} />
+                            <Route exact path="/repos" component={RepoList} />
+                            <Route component={NotFound} />
+                        </Switch>
+                    </BrowserRouter>
+                </Provider>
+            </MuiThemeProvider>
         );
     }
 }
