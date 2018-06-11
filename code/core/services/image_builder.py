ce621af75effbb7336b7922bf75d060f4b9fea89
Richard Hayes <richard.hayes@airfinity.com>
2018-06-11 09:12:35 +0100

    further docker image building changes

--- code/core/services/image_builder.py
+++ code/core/services/image_builder.py
@@ -1,3 +1,5 @@
+import json
+import logging
 import os
 from distutils.dir_util import copy_tree
 from tempfile import TemporaryDirectory
@@ -18,16 +20,23 @@ class ImageBuilder(object):
 
             repo_name = f'{self.registry}/airflow:{tag}'
 
-            self.client.images.build(
-                path=tmp_dir,
-                tag=repo_name,
-                rm=True,
-                forcerm=True
-            )
+            for line in self.client.api.build(path=tmp_dir, tag=repo_name, rm=True, forcerm=True):
+                self._parse_message(line, 'stream')
 
             for line in self.client.images.push(repo_name, stream=True):
-                print(line.strip())
+                self._parse_message(line, 'status')
 
     def generate_config(self):
         # Build Airflow.cfg file for image with creds for database
         pass
+
+    def _parse_message(self, message, key):
+        for line in message.strip().split(b'\r\n'):
+            try:
+                text = json.loads(line)[key]
+                if text != '\n':
+                    print(text)
+            except json.JSONDecodeError:
+                print('ERROR: %s' % message)
+            except KeyError:
+                print(json.loads(line))
