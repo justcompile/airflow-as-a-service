18c51459c09011192e6c34ae37cd66949ab6c550
Richard Hayes <rich@futrli.com>
2018-08-18 20:28:21 +0100

    test coverage for scm/git.py

--- code/scm/tests/test_git_client.py
+++ code/scm/tests/test_git_client.py
@@ -1,6 +1,13 @@
+import ast
+import inspect
 import unittest
 from unittest import mock
 
+from django.apps import apps
+from django.contrib.auth.models import User
+from django.db.models.signals import ModelSignal
+from core import signals
+from core.models import Build, Repository
 from scm.git import GitClient, SSH_KEY_NAME
 
 
@@ -94,6 +101,10 @@ class GitClientMethodsTestCase(BaseTestCase):
             'url': 'http://consul:8200',
         }
 
+        subprocess_patch = mock.patch('scm.git.subprocess')
+        self.subprocess = subprocess_patch.start()
+        self.addCleanup(subprocess_patch.stop)
+
     def test_create_and_save_keys_sends_public_key_to_github(self):
         self.generate_keys.return_value = ('private-key', 'public-key')
         client = GitClient(self.mock_user)
@@ -117,7 +128,57 @@ class GitClientMethodsTestCase(BaseTestCase):
             p_key='private-key',
         )
 
-    # FIXME: TESTS HERE
+    @mock.patch('scm.git.NamedTemporaryFile')
+    @mock.patch('scm.git.cd')
+    def test_checkout_retrieves_private_key_from_consul_and_writes_to_tmp_file(self, mock_cd, mock_tmp_file):
+        client = GitClient(self.mock_user)
+        client._vault_client = mock.Mock()
+        client._vault_client.read.return_value = {
+            'data': {
+                'p_key': 'private',
+            },
+        }
+
+        client.checkout('my-repo', '12345', 'some_dir')
+        self.assertEqual(client._vault_client.read.call_count, 1)
+        mock_tmp_file().__enter__().write.assert_called_with(b'private')
+
+    @mock.patch('scm.git.NamedTemporaryFile')
+    @mock.patch('scm.git.cd')
+    @mock.patch('scm.git.os')
+    def test_checkout_clones_git_repo(self, mock_os, mock_cd, mock_tmp_file):
+        mock_os.environ = {}
+
+        self.settings.GIT_BINARY = 'git'
+
+        client = GitClient(self.mock_user)
+        client._vault_client = mock.Mock()
+        client._vault_client.read.return_value = {
+            'data': {
+                'p_key': 'private',
+            },
+        }
+
+        mock_tmp_file().__enter__().name = 'my-tmp-file'
+
+        client.checkout('my-repo', '12345', 'some_dir')
+        self.subprocess.Popen.assert_has_calls([
+            mock.call(
+                'git clone git@github.com:testuser/my-repo.git some_dir/my-repo'.split(),
+                env={
+                    'GIT_SSH_COMMAND': 'ssh -i my-tmp-file -F /dev/null',
+                },
+            ),
+            mock.call().wait(),
+            mock.call(
+                'git reset --hard 12345'.split(),
+                env={
+                    'GIT_SSH_COMMAND': 'ssh -i my-tmp-file -F /dev/null',
+                },
+            )],
+            mock.call().wait(),
+        )
+
     def test_repo_has_key_returns_false_if_keyname_not_in_github_keys(self):
         self.git().get_user().get_repo().get_keys.return_value = []
 
@@ -127,6 +188,7 @@ class GitClientMethodsTestCase(BaseTestCase):
 
     def test_repo_has_key_returns_true_if_keyname_is_in_github_keys(self):
         self.git().get_user().get_repo().get_keys.return_value = [
+            mock.Mock(title='meh'),
             mock.Mock(title=SSH_KEY_NAME),
         ]
 
@@ -171,3 +233,53 @@ class GitClientMethodsTestCase(BaseTestCase):
             events=["push"],
             active=True,
         )
+
+    def test_create_build_for_latest_commit_returns_build_instance(self):
+        self._disconnect_signals()
+
+        commit = mock.Mock(sha='sha-123')
+        commit.committer.login = 'username'
+
+        self.git().get_user().get_repo().get_commits.return_value = [
+            commit,
+        ]
+
+        repo = Repository.objects.create(
+            owner=User.objects.create_user('myuser'),
+            url='http://myurl.com/my-repo',
+        )
+        client = GitClient(self.mock_user)
+
+        result = client.create_build_for_latest_commit(repo)
+        self.assertIsInstance(result, Build)
+
+    def _disconnect_signals(self):
+        signal_attrs = [
+            x
+            for x in inspect.getmembers(signals)
+            if not x[0].startswith('__')
+        ]
+
+        django_signals = dict(
+            (name, func)
+            for name, func in signal_attrs
+            if isinstance(func, ModelSignal)
+        )
+
+        module_signals = [
+            signal
+            for _, signal in signal_attrs
+            if signal.__module__ == signals.__name__
+        ]
+
+        for signal_func in module_signals:
+            module = ast.parse(inspect.getsource(signal_func))
+            for decorator in module.body[0].decorator_list:
+                if decorator.func.id == 'receiver':
+                    django_signals[decorator.args[0].id].disconnect(
+                        signal_func,
+                        sender=apps.get_model(
+                            app_label=signals.__name__.split('.')[0],
+                            model_name=decorator.keywords[0].value.id,
+                        ),
+                    )
