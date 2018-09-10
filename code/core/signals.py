b1219103ceddd79773d1706ae4011866ba2b7503
Richard Hayes <rich@futrli.com>
2018-09-10 18:06:42 +0100

    nothing to see here

--- code/core/signals.py
+++ code/core/signals.py
@@ -3,7 +3,7 @@ from channels.layers import get_channel_layer
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 
-from core.models import Build, Cluster
+from core.models import Build, BuildLog, Cluster
 from api.serializers import BuildSerializer, ClusterSerializer
 
 
@@ -28,6 +28,29 @@ def send_build_status_to_channel(sender, instance, **kwargs):
             },
         },
     )
+
+
+# @receiver(pre_save, sender=BuildLog)
+# def send_build_log_to_channel(sender, instance, **kwargs):
+#     channel_layer = get_channel_layer()
+
+#     user_id = instance.repository.owner_id
+
+#     try:
+#         Build.objects.get(pk=instance.pk)
+#         action = 'UPDATE'
+#     except Build.DoesNotExist:
+#         action = 'CREATE'
+
+#     async_to_sync(channel_layer.group_send)(
+#         f'builds-{str(user_id)}', {
+#             "type": "build.message",
+#             "message": {
+#                 'action': action,
+#                 'data': BuildSerializer(instance).data,
+#             },
+#         },
+#     )
 
 
 @receiver(pre_save, sender=Cluster)
